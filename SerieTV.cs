08e7541ad97ed67704b852c3a75b1564ed89a888
JUAN RAMON VISCARRA HOLGUIN <14171014@itculiacan.edu.mx>
2020-05-17 17:31:33 -0600

    Juan Ramon - Correccion - Se Agrego Modelo SerieTV

---
+++ SerieTV.cs
@@ -0,0 +1,12 @@
+namespace Parcial1.Models
+ {
+     public class SerieTV
+     {
+         public int Id { get; set; }
+         public string Nombre { get; set; }
+        public string Plataforma { get; set; }  
+         public int Calificacion { get; set; }
+         
+     }
+
+}
