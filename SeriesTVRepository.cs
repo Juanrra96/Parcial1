ab4280976303a1ab353ba67a874addf7261f30ad
JUAN RAMON VISCARRA HOLGUIN <14171014@itculiacan.edu.mx>
2020-05-17 18:28:39 -0600

    Juan Ramon - Sqlite + crear base de datos

--- SeriesTVRepository.cs
+++ SeriesTVRepository.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using Dapper;
+using Microsoft.Data.Sqlite;
 
 namespace Parcial1 {
 
@@ -11,6 +13,20 @@ public class SeriesTVRepository {
         
     }
 
+        public static void IniciarBD(){
+        using (var connection = new SqliteConnection(@"Data Source=mydb.db;"))
+//        ("" +    new SqliteConnectionStringBuilder()  {        DataSource = "data.db"  }))
+        {
+            connection.Open();
+            connection.Execute(
+                @"CREATE TABLE IF NOT EXISTS Leches (
+                        Id INT PRIMARY KEY,
+                        Nombre TEXT NULL,
+                        Plataforma TEXT NULL,
+                        Calificacion INT NULL
+                ) ;");
+        }
+                                    }
         internal List<SerieTV> LeerTodos()
         {
             throw new NotImplementedException();
